02c68907e71740d63190ca5e176c06656aa3daec
Stuart Bowman <stuartsoft2012@gmail.com>
2015-10-07 00:44:35 -0400

    Working on getting the initial matrix all figured out

--- Assets/Cube.cs
+++ Assets/Cube.cs
@@ -1,10 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Cube : MonoBehaviour {
 
-    public enum cubeType {center, edge, corner};
-    public cubeType cType { get; private set; }
+    public enum sides { FRONT=0, RIGHT=1, BACK=2, LEFT=3, TOP=4, BOTTOM=5 };
+    public List<GameObject> panels = new List<GameObject>();
 
+    void Start()
+    {
+        for (int i = 0; i < panels.Count; i++)
+        {
+            //setColor((sides)i, Color.black);
+        }
+    }
 
+    public void setColor(sides side, Color c) {
+        Renderer rend = panels[(int)side].GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.material.color = c;
+    }
+
+    public Color getColor(sides side)
+    {
+        Renderer rend = panels[(int)side].GetComponent<Renderer>();
+        return rend.material.color;
+    }
+    
 }
--- Assets/RubiksCube.cs
+++ Assets/RubiksCube.cs
@@ -9,36 +9,40 @@ public class RubiksCube : MonoBehaviour {
     public List<List<List<GameObject>>> cubePrefabMatrix;
     public List<List<List<Cube>>> cubeRef;
 
-    public enum sides { TOP, BOTTOM, FRONT, BACK, LEFT, RIGHT};
-
-
     // Use this for initialization
     void Start () {
+        StartCoroutine(SetupCube());
+	}
+	
+    IEnumerator SetupCube()
+    {
         cubePrefabMatrix = new List<List<List<GameObject>>>();
         cubeRef = new List<List<List<Cube>>>();
 
-	    for (int x = 0; x < 3; x++)
+        for (int x = 0; x < 3; x++)
         {
             List<List<GameObject>> GOPlane = new List<List<GameObject>>();
-            List<List<Cube>> CubePlane= new List<List<Cube>>();
+            List<List<Cube>> CubePlane = new List<List<Cube>>();
             for (int y = 0; y < 3; y++)
             {
                 List<GameObject> GORow = new List<GameObject>();
-                List<Cube> CubeRow = new List<Cube>(); 
+                List<Cube> CubeRow = new List<Cube>();
                 for (int z = 0; z < 3; z++)
                 {
+                    yield return new WaitForSeconds(0.25f);
                     GameObject cubePrefab = Instantiate(CubePrefab, Vector3.zero, Quaternion.identity) as GameObject;
                     cubePrefab.transform.SetParent(transform);
                     float spacing = 1.05f;
-                    cubePrefab.transform.position = new Vector3((x-1), (y-1), (z-1))*spacing;
+                    cubePrefab.transform.position = new Vector3((x - 1), (y - 1), (z - 1)) * spacing;
                     Renderer rend = cubePrefab.GetComponent<Renderer>();
                     rend.enabled = true;
                     rend.material.color = Color.black;
 
                     Cube temp = cubePrefab.GetComponent<Cube>();
+                    //temp.setFrontPanelColor(Color.white);
                     GORow.Add(cubePrefab);
                     CubeRow.Add(temp);
-
+                    
                 }
                 GOPlane.Add(GORow);
                 CubePlane.Add(CubeRow);
@@ -47,10 +51,10 @@ public class RubiksCube : MonoBehaviour {
             cubeRef.Add(CubePlane);
         }
         //transform.rotation = Quaternion.Euler(new Vector3(45,0,45));
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update () {
         //transform.Rotate(Time.deltaTime * 10, Time.deltaTime * 10, 0.0f);
 	}
 }
